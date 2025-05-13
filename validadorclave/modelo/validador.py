fce385fc9f93ae91d4660145be943f6f63359f91
Juan Esteban Correa <correaguzmanjuanesteban@gmail.com>
2025-05-12 21:09:56 -0500

    se hace la ultima clase, se inicializan los atributos y se hace una validacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -50,3 +50,17 @@ class ReglaValidacionGanimedes(ReglaValidacion):
 class ReglaValidacionCalisto(ReglaValidacion):
     def __init__(self):
         super().__init__(6)
+
+    def contiene_calisto(self, clave):
+        pass
+
+    def es_valida(self, clave):
+        return True
+
+
+class Validador:
+    def __init__(self, regla):
+        self.regla = regla
+
+    def es_valida(self, clave):
+        return self.regla.es_valida(clave)
